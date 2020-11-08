96f110ae9d95cdf9f78e1a53eb118cc89050697b
pauloplr <pauloroberto_nobrega@hotmail.com>
2020-11-07 22:04:14 -0300

    Adicionando o código

---
+++ sketch_nov04b/sketch_nov04b.ino
@@ -0,0 +1,77 @@
+#define GPS_RX 0
+#define GPS_TX 3
+#define GPS_Serial_Baud 9600
+#include <SoftwareSerial.h>
+#include <TinyGPS.h>
+ 
+TinyGPS gps;
+ 
+//SoftwareSerial gpsSerial(GPS_RX, GPS_TX);
+ 
+void setup() {
+  Serial.begin(GPS_Serial_Baud);
+  //gpsSerial.begin(GPS_Serial_Baud);
+  //pinMode(LED_BUILTIN, OUTPUT);
+}
+ 
+void loop() {
+  bool newData = false;
+  unsigned long chars;
+  // For one second we parse GPS data and report some key values
+  for (unsigned long start = millis(); millis() - start < 1000;) {
+    /*while (gpsSerial.available()) {
+      char c = gpsSerial.read();
+      // Serial.write(c); //apague o comentario para mostrar os dados crus
+      if (gps.encode(c)) // Atribui true para newData caso novos dados sejam recebidos
+        newData = true;
+    }*/
+    while (Serial.available()) {
+      char c = Serial.read();
+      if (gps.encode(c)) // Atribui true para newData caso novos dados sejam recebidos
+        newData = true;
+    }
+  }
+  if (newData) {
+    /*digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+    delay(1000);                       // wait for a second
+    digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+    delay(1000);*/
+    float flat, flon;
+    unsigned long age;
+    gps.f_get_position(&flat, &flon, &age);
+    Serial.print("LAT=");
+    Serial.print(flat == TinyGPS::GPS_INVALID_F_ANGLE ? 0.0 : flat, 6);
+    Serial.print(" LON=");
+    Serial.print(flon == TinyGPS::GPS_INVALID_F_ANGLE ? 0.0 : flon, 6);
+    Serial.print(" SAT=");
+    Serial.print(gps.satellites() == TinyGPS::GPS_INVALID_SATELLITES ? 0 : gps.satellites());
+    Serial.print(" PREC=");
+    Serial.print(gps.hdop() == TinyGPS::GPS_INVALID_HDOP ? 0 : gps.hdop());
+    Serial.println();
+    
+     //Dia e Hora
+     int ano;
+     long latitude, longitude;
+     unsigned long idadeInfo;
+     byte mes, dia, hora, minuto, segundo, centesimo;
+     gps.crack_datetime(&ano, &mes, &dia, &hora, &minuto, &segundo, &centesimo, &idadeInfo);
+
+     Serial.print("Data (GMT): ");
+     Serial.print(dia);
+     Serial.print("/");
+     Serial.print(mes);
+     Serial.print("/");
+     Serial.println(ano);
+
+     Serial.print("Horario (GMT): ");
+     Serial.print(hora);
+     Serial.print(":");
+     Serial.print(minuto);
+     Serial.print(":");
+     Serial.print(segundo);
+     Serial.print(":");
+     Serial.println(centesimo);
+    Serial.println();
+    Serial.println();
+  }
+}
