ebb88de484f6566bd6fd8675ebf6bfcb4ebda469
pauloplr <pauloroberto_nobrega@hotmail.com>
2020-12-02 17:25:34 -0300

    Adicionado versão sem TinyGPS

--- sketch_nov04b_sem_tiny_GPS/sketch_nov04b_sem_tiny_GPS.ino
+++ sketch_nov04b_sem_tiny_GPS/sketch_nov04b_sem_tiny_GPS.ino
@@ -1,15 +1,11 @@
-#define GPS_RX 0
-#define GPS_TX 3
+//#define GPS_RX 0
+//#define GPS_TX 3
 #define GPS_Serial_Baud 9600
-#include <SoftwareSerial.h>
-#include <TinyGPS.h>
+//#include <SoftwareSerial.h>
+//#include <TinyGPS.h>
 int estado_btn; //Variavel para ler o status do pushbutton
 int cont = 1;
 const int Botao = 3; //botao no pino 1
- 
-TinyGPS gps;
- 
-//SoftwareSerial gpsSerial(GPS_RX, GPS_TX);
  
 void setup() {
   Serial.begin(GPS_Serial_Baud);
@@ -20,39 +16,41 @@ void setup() {
 }
  
 void loop() {   
-    bool newData = false;
-    unsigned long chars;
+    //bool newData = false;
+    //unsigned long chars;
     // For one second we parse GPS data and report some key values
-    for (unsigned long start = millis(); millis() - start < 1000;) {
+    //for (unsigned long start = millis(); millis() - start < 1000;) {
       /*while (gpsSerial.available()) {
         char c = gpsSerial.read();
         // Serial.write(c); //apague o comentario para mostrar os dados crus
         if (gps.encode(c)) // Atribui true para newData caso novos dados sejam recebidos
           newData = true;
       }*/
-      while (Serial.available()) {
+      /*while (Serial.available()) {
         char c = Serial.read();
         if (gps.encode(c)) // Atribui true para newData caso novos dados sejam recebidos
           newData = true;
-      }
-    }
-    if (newData) {
+      }*/
+    //}
+    //if (Serial.available()) {
       estado_btn = digitalRead(Botao); //le o estado do botão - HIGH OU LOW
       if (estado_btn == HIGH){
         cont++;
       }
       if (cont % 2 == 0){ //Se botão estiver pressionado (HIGH)
           digitalWrite(LED_BUILTIN, HIGH); // acende o led do pino 2.
+          if (Serial.available()) {
+            char c = Serial.read();
+            Serial.print(c);
+          }
           /*digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
           delay(1000);                       // wait for a second
           digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
           delay(1000);*/
-          float flat, flon;
+          /*float flat, flon;
           unsigned long age;
           gps.f_get_position(&flat, &flon, &age);
-          char c = Serial.read();
-          Serial.print(c);
-          /*Serial.print("LAT=");
+          Serial.print("LAT=");
           Serial.print(flat == TinyGPS::GPS_INVALID_F_ANGLE ? 0.0 : flat, 6);
           Serial.print(" LON=");
           Serial.print(flon == TinyGPS::GPS_INVALID_F_ANGLE ? 0.0 : flon, 6);
@@ -89,5 +87,5 @@ void loop() {
     } else { //se não estiver pressionado (LOW)
           digitalWrite(LED_BUILTIN, LOW); //deixa o led do pino 2 apagado
     }
-  }
+  //}
 }
